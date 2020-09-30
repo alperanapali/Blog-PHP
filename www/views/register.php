5e5620b1e3a02421bb235440ebf6426a0e83315a
Alper Anapali <anapalialper@gmail.com>
2020-09-30 15:59:45 +0300

    register

--- www/views/register.php
+++ www/views/register.php
@@ -1,44 +1,51 @@
 <?php
 $page_heading = "Register Now!";
 include 'includes/header.php';
-include 'includes/connect.php';
+include 'includes/connectPDO.php';
 
 function registry_entries_modifier($entry, $password = false)
 {
     $entry = strip_tags($entry); //removes tags for unwanted html
 
-    if(!$password){ // does not remove the blanks if its password since it might be the users choice
+    if (!$password) { // does not remove the blanks if its password since it might be the users choice
         $entry = str_replace(' ', '', $entry); //removes blanks
         $entry = ucfirst(strtolower($entry)); // lower case , first letter upper
     }
     return $entry;
 }
 
-function does_entry_exist($username, $email)
+function does_entry_exist($username, $email, $conn)
 {
+    $sql = "SELECT * FROM users WHERE username='$username' OR  email='$email'";
+    $stmt = $conn->query($sql);
+    $r = $stmt->fetch();
+    if ($r['username'] === $username) {
+        echo "Password already exist";
+    } else if ($r['email'] === $email) {
+        echo "Email already exist";
+    }
 
-
+    echo $r['username'], $username;
 }
 
 function does_registry_entries_match($entry, $confirm_entry, $type = ['password', 'email'])
 {
-    if($entry == $confirm_entry){
-        if($type == 'email'){
+    if ($entry == $confirm_entry) {
+        if ($type == 'email') {
             filter_var($entry, FILTER_VALIDATE_EMAIL);
             return $entry;
         }
-        if($type == 'password'){
+        if ($type == 'password') {
             return $entry;
         }
-    } else{
-        echo $type."s does not match <br>";
+    } else {
+        echo  $type." does not match";
     }
 
 }
 
 
-
-if(isset($_POST['reg'])){
+if (isset($_POST['reg'])) {
 
     $username = registry_entries_modifier($_POST['username']);
     $email = registry_entries_modifier($_POST['email']);
@@ -49,27 +56,26 @@ if(isset($_POST['reg'])){
     $em = does_registry_entries_match($email, $confirm_email, 'email');
     $pass = does_registry_entries_match($password, $confirm_password, 'password');
 
-
-
-
+    does_entry_exist($username, $email, $conn);
 
 }
+
 
 ?>
 
 
 <div class="container-md" align="center"><h3>Register</h3></div>
-<form action="register.php" method="POST" class="container" align="center">
-    <input type="text" name="username" class="w3-input w3-border" placeholder="User Name">
+<form action="/register" method="POST" class="container" align="center">
+    <input type="text" name="username" class="w3-input w3-border" placeholder="User Name" required>
     <br>
     <br>
-    <input type="email" name="email" class="card-text" placeholder="Email">
-    <input type="email" name="confirm_email" class="card-text" placeholder=" Confirm Email">
+    <input type="email" name="email" class="card-text" placeholder="Email" required>
+    <input type="email" name="confirm_email" class="card-text" placeholder=" Confirm Email" required>
     <br>
     <br>
 
-    <input type="password" name="password" class="card-text" placeholder="Password">
-    <input type="password" name="confirm_password" class="card-text" placeholder="Confirm Password">
+    <input type="password" name="password" class="card-text" placeholder="Password" required>
+    <input type="password" name="confirm_password" class="card-text" placeholder="Confirm Password" required>
     <br>
     <br>
     <input type="submit" name="reg" value="Register" class="btn btn-primary">
